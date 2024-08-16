6229481231a33329d294e6d64b67b1db60834abe
naitik-yadav <ntkydv16@gmail.com>
2024-08-16 16:07:19 +0530

    update host

--- Student-Management-System/student_management_system/settings.py
+++ Student-Management-System/student_management_system/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = '(i#*06f#keydy_fh17bf=$0f6v)^wr^l7*u4gq42m*sztu#2_m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["Naitik2000.pythonanywhere.com", "localhost"]
+ALLOWED_HOSTS = ["naitik-management-system-bice.vercel.app", "localhost"]
 
 
 # Application definition
